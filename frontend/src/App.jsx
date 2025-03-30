2dc65cdf11d39ddbff3df148f158e03adb16a792
piyush192004 <pksingh84048@gmail.com>
2025-03-30 10:50:22 +0530

    Footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,6 +9,7 @@ import MyProfile from "./pages/MyProfile";
 import MyAppointment from "./pages/MyAppointment";
 import Appointment from "./pages/Appointment";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 const App = () => (
   <div className="mx-4 sm:mx-[10%]">
@@ -24,6 +25,7 @@ const App = () => (
       <Route path="/MyAppointment" element={<MyAppointment />} />
       <Route path="/Appointment/:docID" element={<Appointment />} />
     </Routes>
+    <Footer />
   </div>
 );
 
--- frontend/src/pages/Doctors.jsx
+++ frontend/src/pages/Doctors.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Doctors = () => {
-  return <div>Doctor</div>;
+  return <div></div>;
 };
 
 export default Doctors;
